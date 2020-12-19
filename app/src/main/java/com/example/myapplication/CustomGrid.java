719993cb2b42befca142e9d2b354c98ad70a15c4
kaushalpurohit <kpurohit43@gmail.com>
2020-12-20 02:50:35 +0530

    Add search results grid

---
+++ app/src/main/java/com/example/myapplication/CustomGrid.java
@@ -0,0 +1,68 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+public class CustomGrid extends BaseAdapter{
+    private Context mContext;
+    private String[] web;
+    private String[] Imageid;
+
+    public CustomGrid(Context c,String[] web,String[] Imageid ) {
+        mContext = c;
+        this.Imageid = Imageid;
+        this.web = web;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return web.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            grid = new View(mContext);
+            grid = inflater.inflate(R.layout.grid_single, null);
+            TextView textView = (TextView) grid.findViewById(R.id.grid_text);
+            ImageView imageView = (ImageView)grid.findViewById(R.id.grid_image);
+            textView.setText(web[position]);
+            Log.i("message", String.valueOf(position));
+            Picasso
+                    .with(mContext)
+                    .load(Imageid[position])
+                    .fit() // will explain later
+                    .into(imageView);
+        } else {
+            grid = (View) convertView;
+        }
+
+        return grid;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import org.json.JSONException;
+import android.widget.AdapterView;
+import android.widget.SearchView;
+import android.widget.GridView;
 import org.json.JSONObject;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -22,50 +22,101 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     int i = 0;
+    GridView grid;
+    public ArrayList<String> linksList = new ArrayList<String>();
+    public String[] imagesList = new String[10];
+    public String[] titlesList = new String[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void sendMessage(View view) {
-        EditText text = (EditText)findViewById(R.id.searchText);
-        String search = text.getText().toString();
-        String url = "https://bookdl-api.herokuapp.com/search?book=" + search;
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        client.newCall(request).enqueue(new Callback() {
+        getSupportActionBar().hide();
+        SearchView text = (SearchView) findViewById(R.id.searchText);
+        text.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    final String myResponse = response.body().string();
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                ArrayList<String> result = new ArrayList<String>();
-                                for(int i=1; i<12; i++) {
-                                    JSONObject res = (new JSONObject(myResponse)).getJSONObject(String.valueOf(i));
-                                    String title = res.getString("Title");
-                                    result.add(title);
+            public boolean onQueryTextSubmit(String search) {
+                String url = "https://bookdl-api.herokuapp.com/search?book=" + search;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        for(int i=1; i<11; i++) {
+                                            JSONObject res = (new JSONObject(myResponse)).getJSONObject(String.valueOf(i));
+                                            String title = res.getString("Title");
+                                            String link = res.getString("Link");
+                                            String image = res.getString("image");
+                                            imagesList[i-1] = image;
+                                            titlesList[i-1] = title;
+                                            linksList.add(link);
+                                        }
+                                        CustomGrid adapter = new CustomGrid(MainActivity.this, titlesList, imagesList);
+                                        grid=(GridView)findViewById(R.id.grid);
+                                        grid.setAdapter(adapter);
+                                    }
+                                    catch (Exception e){
+                                        Log.i("exception", "1", e);
+                                    }
                                 }
-                                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.activity_listview, R.id.itemTextView, result);
-
-                                ListView listView = (ListView) findViewById(R.id.booklist);
-                                listView.setAdapter(arrayAdapter);
-                            }
-                            catch (Exception e){
-                                Log.i("exception", "1", e);
-                            }
+                            });
                         }
-                    });
-                }
+                    }
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
     }
+
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener() {
+      public void onItemClick(AdapterView parent, View v, int position, long id){
+          Log.i("Item:", String.valueOf(position));
+          String url = "https://bookdl-api.herokuapp.com/download?url=" + linksList.get(position);
+          OkHttpClient client = new OkHttpClient();
+          Request request = new Request.Builder()
+                  .url(url)
+                  .build();
+          client.newCall(request).enqueue(new Callback() {
+              @Override
+              public void onFailure(Call call, IOException e) {
+                  e.printStackTrace();
+              }
+              @Override
+              public void onResponse(Call call, Response response) throws IOException {
+                  if (response.isSuccessful()) {
+                      final String myResponse = response.body().string();
+                      MainActivity.this.runOnUiThread(new Runnable() {
+                          @Override
+                          public void run() {
+                              try {
+                                      JSONObject res = (new JSONObject(myResponse));
+                                      String url = res.getString("link");
+                                      Log.i("URL:", url);
+                                  }
+                              catch (Exception e){
+                                  Log.i("exception", "1", e);
+                              }
+                          }
+                      });
+                  }
+              }
+          });
+      }
+    };
 }
