67788bb49ea40b95a6799aa815e0876fe52296aa
kaushalpurohit <kpurohit43@gmail.com>
2020-12-21 16:13:46 +0530

    Add final search activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,15 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
-import android.widget.GridView;
 import org.json.JSONObject;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -22,19 +19,22 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     int i = 0;
-    GridView grid;
     public ArrayList<String> linksList = new ArrayList<String>();
     public String[] imagesList = new String[10];
     public String[] titlesList = new String[10];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         SearchView text = (SearchView) findViewById(R.id.searchText);
+        ProgressBar spinner=(ProgressBar)findViewById(R.id.progress);
+        spinner.setVisibility(View.GONE);
         text.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String search) {
+                spinner.setVisibility(View.VISIBLE);
                 String url = "https://bookdl-api.herokuapp.com/search?book=" + search;
                 OkHttpClient client = new OkHttpClient();
                 Request request = new Request.Builder()
@@ -63,8 +63,11 @@ public class MainActivity extends AppCompatActivity {
                                             linksList.add(link);
                                         }
                                         CustomGrid adapter = new CustomGrid(MainActivity.this, titlesList, imagesList);
-                                        grid=(GridView)findViewById(R.id.grid);
-                                        grid.setAdapter(adapter);
+                                        ExpandableHeightGridView exGrid=(ExpandableHeightGridView) findViewById(R.id.grid);
+                                        exGrid.setNumColumns(2);
+                                        exGrid.setAdapter(adapter);
+                                        exGrid.setExpanded(true);
+                                        spinner.setVisibility(View.GONE);
                                     }
                                     catch (Exception e){
                                         Log.i("exception", "1", e);
@@ -79,8 +82,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                grid=(GridView)findViewById(R.id.grid);
-                grid.setAdapter(null);
+                ExpandableHeightGridView exGrid=(ExpandableHeightGridView) findViewById(R.id.grid);
+                exGrid.setAdapter(null);
                 return false;
             }
         });
